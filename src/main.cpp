29d5eec7c336335bc20cd36d98a88666c9cc6294
Garchiza <deltastaraquila@gmail.com>
2024-06-02 20:01:10 +0300

    WOW NEW TRIANGLE YAY (perelivaushiysya\)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
-#include <glad/glad.h>                                                                  //Библиотека GLAD
-#include <GLFW/glfw3.h>                                                                 //Библиотека GLFW
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+
+
+GLfloat point[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+   -0.5f, -0.5f, 0.0f
+};
+GLfloat color[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"    color = vertex_color;"
+"    gl_Position = vec4(vertex_position, 1.0);"
+"}";
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"    frag_color = vec4(color, 1.0);"
+"}";
 
 int WindowSizeX = 640, WindowSizeY = 480;
 
@@ -16,22 +45,20 @@ void WndwSizeCallback(GLFWwindow* pWindow, int x, int y) {
     glViewport(0,0, WindowSizeX, WindowSizeY);
 }
 
-int main(void)
-{
-    if (!glfwInit()) {                                                                  //Если не получилось инициализировать glfw, программа закрывает с ошибкой
+int main(void) {
+    if (!glfwInit()) {
         std::cout << "!!!GLFW initalisation error!!!";
         return -1;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);					                    //Префикс версии OpenGL которую мы будем использовать (4.6);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);					                    //Постфикс версии OpenGL которую мы будем использовать (4.6). Таким образом можно уверить себя в том, что ты можешь использовать весь функционал версии 4.6
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);                      //Какая-та залупа, но нужная
 
+    GLFWwindow* pWindow = glfwCreateWindow(WindowSizeX, WindowSizeY, "Battle tanks", NULL, NULL);
 
-    GLFWwindow* pWindow = glfwCreateWindow(WindowSizeX, WindowSizeY, "Battle tanks", NULL, NULL);	    //Создать окно. Ширина 640, высота 480, имя в ковычках, полноэкранный режим: NULL, ещё какой-то режим: NULL
-
-    if (!pWindow) {									                                    //Проверка, создано ли окно или нет. (На этот раз с ошибкой!)
+    if (!pWindow) {
         std::cout << "!!!window creation error!!!";
         glfwTerminate();
         return -1;
@@ -40,28 +67,69 @@ int main(void)
     glfwSetWindowSizeCallback(pWindow, WndwSizeCallback);
     glfwSetKeyCallback(pWindow, KeyCallback);
 
-    glfwMakeContextCurrent(pWindow);					                    	        //Указатель, с каким окном ведётся работа
+    glfwMakeContextCurrent(pWindow);
 
 
-    if (!gladLoadGL()) {							                                    //Инициализация OpenGL GLAD с ошибкой
+    if (!gladLoadGL()) {
         std::cout << "!!!GLAD initialisation error!!!";
         return -1;
     }
 
 
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;		            //Вывести название видеокарты
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;	        //Вывести версию OpenGL
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
 
-    glClearColor(1, 1, 0, 1);							                                //Назначить цвет окна
+    glClearColor(1, 1, 0, 1);
 
-    while (!glfwWindowShouldClose(pWindow)) {				                    	    //Цикл отрисовки окна, который продолжается до тех пор, пока пользователь это окно не закроет.
-        glClear(GL_COLOR_BUFFER_BIT);						                            //Очищает буфер цвета
-        glfwSwapBuffers(pWindow);							                            //Меняет местами задний и передний буфер цвета (чаго). Ну короче буфера нужны чтобы помочь с проблемой разрывов кадров. Эта система называется double buffering
-        glfwPollEvents();								                                //Позволяет glfw работать с приходящими от пользователя ивентами
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+
+    while (!glfwWindowShouldClose(pWindow)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(pWindow);
+        glfwPollEvents();
     }
 
-
-    glfwTerminate();			                                                        //В коем-то веке закрыть программу
+    glfwTerminate();
     return 0;
 }
