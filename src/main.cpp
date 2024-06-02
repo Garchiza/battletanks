96b078ff55441396e4bf349e210ca197859e85de
Garchiza <deltastaraquila@gmail.com>
2024-06-02 14:00:39 +0300

    Added keys, window size change, brand new yellow color

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,67 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <glad/glad.h>                                                                  //Библиотека GLAD
+#include <GLFW/glfw3.h>                                                                 //Библиотека GLFW
 #include <iostream>
+
+int WindowSizeX = 640, WindowSizeY = 480;
+
+void KeyCallback(GLFWwindow* pWindow, int key, int scncd, int act, int mode) {
+    if (key == GLFW_KEY_ESCAPE && act == GLFW_PRESS){
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+}
+
+void WndwSizeCallback(GLFWwindow* pWindow, int x, int y) {
+    WindowSizeX = x;
+    WindowSizeY = y;
+    glViewport(0,0, WindowSizeX, WindowSizeY);
+}
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {                                                                  //Если не получилось инициализировать glfw, программа закрывает с ошибкой
+        std::cout << "!!!GLFW initalisation error!!!";
         return -1;
+    }
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);					                    //Префикс версии OpenGL которую мы будем использовать (4.6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);					                    //Постфикс версии OpenGL которую мы будем использовать (4.6). Таким образом можно уверить себя в том, что ты можешь использовать весь функционал версии 4.6
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);                      //Какая-та залупа, но нужная
+
+
+    GLFWwindow* pWindow = glfwCreateWindow(WindowSizeX, WindowSizeY, "Battle tanks", NULL, NULL);	    //Создать окно. Ширина 640, высота 480, имя в ковычках, полноэкранный режим: NULL, ещё какой-то режим: NULL
+
+    if (!pWindow) {									                                    //Проверка, создано ли окно или нет. (На этот раз с ошибкой!)
+        std::cout << "!!!window creation error!!!";
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(pWindow, WndwSizeCallback);
+    glfwSetKeyCallback(pWindow, KeyCallback);
 
-    if (!gladLoadGL()) {
-        std::cout << "whoopsy, its an error! now fuck off, please";
+    glfwMakeContextCurrent(pWindow);					                    	        //Указатель, с каким окном ведётся работа
+
+
+    if (!gladLoadGL()) {							                                    //Инициализация OpenGL GLAD с ошибкой
+        std::cout << "!!!GLAD initialisation error!!!";
         return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor;
 
-    glClearColor(0, 1, 0, 1);
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;		            //Вывести название видеокарты
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;	        //Вывести версию OpenGL
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+    glClearColor(1, 1, 0, 1);							                                //Назначить цвет окна
+
+    while (!glfwWindowShouldClose(pWindow)) {				                    	    //Цикл отрисовки окна, который продолжается до тех пор, пока пользователь это окно не закроет.
+        glClear(GL_COLOR_BUFFER_BIT);						                            //Очищает буфер цвета
+        glfwSwapBuffers(pWindow);							                            //Меняет местами задний и передний буфер цвета (чаго). Ну короче буфера нужны чтобы помочь с проблемой разрывов кадров. Эта система называется double buffering
+        glfwPollEvents();								                                //Позволяет glfw работать с приходящими от пользователя ивентами
     }
 
-    glfwTerminate();
+
+    glfwTerminate();			                                                        //В коем-то веке закрыть программу
     return 0;
 }
